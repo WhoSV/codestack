02e2d340931b9d33d7ed18dfba5cb0f69ae752c9
WhoSV <vladislavsiumbeli@gmail.com>
2018-02-27 14:58:07 +0200

    feat(ActiveCourse): style sidebar

--- app/components/Dashboard/ActiveCourse/Sidebar/index.jsx
+++ app/components/Dashboard/ActiveCourse/Sidebar/index.jsx
@@ -3,17 +3,51 @@ import React from 'react'
 // Component Style
 import style from './style.less'
 
+const chapterList = [
+	{
+		title: "Introduction",
+	},
+	{
+		title: "Chapter 1",
+	},
+	{
+		title: "Chapter 2",
+	},
+	{
+		title: "Chapter 3",
+	},
+	{
+		title: "Chapter 4",
+	},
+	{
+		title: "Chapter 5",
+	},
+]
+
 class Sidebar extends React.Component {
 	constructor(props) {
 		super()
 		this.state = {
 		}
+		this.navigateToChapter = this.navigateToChapter.bind(this)
   }
+
+	navigateToChapter(){
+		console.log("go to chapter");
+	}
 
   render (){
 		return (
 			<div className={style.sidebar}>
-	      <h3 className={style.title}>Sidebar</h3>
+	      <h4 className={style.title}>Learn C#</h4>
+
+				{chapterList.map((chapter, index) => {
+					return (
+						<a key={index} className={style.chapterContainer} onClick={this.navigateToChapter}>
+							{chapter.title}
+						</a>
+					)
+				})}
 			</div>
 	  )
 	}
