6aa4b6fb422e2d475af924408c58c3e48bbdea76
WhoSV <vladislavsiumbeli@gmail.com>
2018-03-20 11:02:02 +0200

    feat(Dashboard): add teacher filed to courses list

--- app/components/Dashboard/Courses/CourseList/index.jsx
+++ app/components/Dashboard/Courses/CourseList/index.jsx
@@ -7,41 +7,49 @@ const courses = [
   {
     title: "C#",
     rating: "15",
+    teacher: "Cristian Cimpoesh",
     date: "12/21/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "Swift",
     rating: "62",
+    teacher: "Cristian Cimpoesh",
     date: "11/30/15",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "Ruby",
     rating: "55",
+    teacher: "Cristian Cimpoesh",
     date: "1/23/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "JavaScript",
     rating: "85",
+    teacher: "Cristian Cimpoesh",
     date: "11/11/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "C#",
     rating: "15",
+    teacher: "Cristian Cimpoesh",
     date: "12/21/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "Swift",
     rating: "62",
+    teacher: "Cristian Cimpoesh",
     date: "11/30/15",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "Ruby",
     rating: "55",
+    teacher: "Cristian Cimpoesh",
     date: "1/23/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }, {
     title: "JavaScript",
     rating: "85",
+    teacher: "Cristian Cimpoesh",
     date: "11/11/18",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non augue quis dolor congue lacinia in pretium nunc. Pellentesque commodo vehicula leo, nec pretium libero consequat varius. Aliquam varius tellus quis magna dignissim, non molestie mauris rutrum. In viverra nunc odio, id gravida erat faucibus id."
   }
@@ -74,18 +82,21 @@ class CourseList extends React.Component {
       <div className={style.courseList}>
         {
           courses.map((course, index) => {
-            return (<div key={index} className={style.listItem}>
-              <div className={style.listItemTitle}>
-                <a onClick={this.navigateToCourse}>
-                  <h3><img className={style.defaultIconStyle} src={img.defaultIcon}/>{course.title}</h3>
-                </a>
-                <a className={style.upVoteStyle} onClick={this.upVote}>
-                  <h5><ThumbUpIcon className={style.upvoteIconStyle}/>{course.rating}Likes</h5>
-                </a>
+            return (
+              <div key={index} className={style.listItem}>
+                <div className={style.listItemTitle}>
+                  <a onClick={this.navigateToCourse}>
+                    <h3><img className={style.defaultIconStyle} src={img.defaultIcon}/>{course.title}</h3>
+                  </a>
+                  <a className={style.upVoteStyle} onClick={this.upVote}>
+                    <h5><ThumbUpIcon className={style.upvoteIconStyle}/>{course.rating} Likes</h5>
+                  </a>
+                </div>
+                <h5 className={style.listItemTeacher}>by: {course.teacher}</h5>
+                <h5 className={style.listItemDate}>{course.date}</h5>
+                <p className={style.listItemDescription}>{course.description}</p>
               </div>
-              <h5 className={style.listItemDate}>{course.date}</h5>
-              <p className={style.listItemDescription}>{course.description}</p>
-            </div>)
+            )
           })
         }
       </div>
